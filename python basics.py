4c64ed6528b37ea474b50d1a2ea7a73159ce866b
ARCHITA2405 <archita0740@gmail.com>
2026-09-16 10:00:15 +0530

    Add python basics

---
+++ python basics.py
@@ -0,0 +1,14 @@
+name="Archita"
+age = 18
+percentage=85.5
+student = True
+course = "B.Sc computer science with cyber security"
+year = 2
+learning_python =True
+print("Name:",name)
+print("Age:",age)
+print("Course:",course)
+print("Year:",year)
+print("Student :",student)
+print("Percentage:",percentage)
+print("Learning_python:", learning_python)
